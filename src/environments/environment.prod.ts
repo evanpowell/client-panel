00664558b639f434ac363f596e054a469e022a41
evanpowell <evanpowellmusic@gmail.com>
2018-11-06 10:12:20 -0600

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBchqJdMcNLWsP4SZu2FXN9xn_NBCkF7VA",
+    authDomain: "clientpanelprod-4.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-4.firebaseio.com",
+    projectId: "clientpanelprod-4",
+    storageBucket: "clientpanelprod-4.appspot.com",
+    messagingSenderId: "867399517248"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBchqJdMcNLWsP4SZu2FXN9xn_NBCkF7VA",
+    authDomain: "clientpanelprod-4.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-4.firebaseio.com",
+    projectId: "clientpanelprod-4",
+    storageBucket: "clientpanelprod-4.appspot.com",
+    messagingSenderId: "867399517248"
+  }
 };
 
 /*
